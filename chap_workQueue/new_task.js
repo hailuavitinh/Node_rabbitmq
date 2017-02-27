883b6ae42f17e5d4682c9874d585210dd16b606c
Dang Thanh <dangthanh@Dangs-MacBook-Pro.local>
2017-02-27 11:16:26 +0700

    fix chap 2

--- chap_workQueue/new_task.js
+++ chap_workQueue/new_task.js
@@ -3,7 +3,7 @@ var amqp = require("amqplib/callback_api");
 amqp.connect("amqp://thanhdc:abc@123@104.223.20.159",function(err,conn){
     conn.createChannel(function(err,ch){
         var q = "task_queue";
-        var mwg = process.argv.slice(2).join(' ') || "Hello World!";
+        var msg = process.argv.slice(2).join(' ') || "Hello World!";
         
         ch.assertQueue(q,{durable:true});
         ch.sendToQueue(q,new Buffer(msg),{persistent:true});
