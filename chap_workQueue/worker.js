aa966491997db4bbd8aa17f885f1cabad1d22110
Dang Thanh <dangthanh@Dangs-MacBook-Pro.local>
2017-02-27 11:14:25 +0700

    fix chap 2

---
+++ chap_workQueue/worker.js
@@ -0,0 +1,17 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://thanhdc@abc@123@104.223.20.159",function(err,conn){
+    conn.createChannel(function(err,ch){
+        var q = "task_queue";
+        ch.assertQueue(q,{durable:true});
+        
+        ch.consume(q,function(msg){
+            var secs  = msg.content.toString('.'.length - 1);
+
+            console.log("[x] Received %s",msg.content.toString());
+            setTimeout(function(){
+                console.log("[x] Done");
+            },secs*1000);
+        },{noAck:true});
+    });
+});
