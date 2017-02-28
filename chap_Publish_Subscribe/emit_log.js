6ea2a5d1a65ae14ed39b03be79512863436f0c25
Dang Chi Thanh <dang.phongvan.thanh@gmail.com>
2017-02-28 15:14:45 +0700

    fix emit

--- chap_Publish_Subscribe/emit_log.js
+++ chap_Publish_Subscribe/emit_log.js
@@ -4,14 +4,16 @@ amqp.connect("amqp://thanhdc:abc@123@104.223.20.159",function(err,conn){
     conn.createChannel(function(err,ch){
         var ex = "logs";
         var msg = process.argv.slice(2).join(' ') || "Hello World!";
-
+        console.log(process.argv);
+        console.log(process.argv.slice(2));
+        
         ch.assertExchange(ex,"fanout",{durable:false});
-        ch.publish(ex,"",new Buffer(msg));
+        ch.publish(ex,"pub",new Buffer(msg));
         console.log("[x] Sent %s",msg);
     });
 
     setTimeout(function(){
         conn.close();
         process.exit(0)
-    },500)
+    },50000)
 });
