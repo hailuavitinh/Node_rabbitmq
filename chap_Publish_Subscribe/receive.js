f7dae3a1cac1ae5b8b9404a6352bd9b7c72cd411
Dang Thanh <dangthanh@Dangs-MacBook-Pro.local>
2017-02-28 15:15:22 +0700

    fix receive

--- chap_Publish_Subscribe/receive.js
+++ chap_Publish_Subscribe/receive.js
@@ -3,10 +3,11 @@ var amqp = require("amqplib/callback_api");
 amqp.connect("amqp://thanhdc:abc@123@104.223.20.159",function(err,conn){
     conn.createChannel(function(err,ch){
         var ex = "logs";
+        var key_binding = process.argv.slice(2).join(' ');
         ch.assertExchange(ex,"fanout",{durable:false});
         ch.assertQueue("",{exclusive:true},function(err,q){
             console.log("[*] Waiting for message in %s. To exit press CTRL + C",q.queue);
-            ch.bindQueue(q.queue,ex,'');
+            ch.bindQueue(q.queue,ex,key_binding);
             ch.consume(q.queue,function(msg){
                 console.log("[x] %s",msg.content.toString())
 
